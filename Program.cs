79e135d37ddd1d46e8e064a0678057ad8b7d2e60
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-02-15 17:16:24 +0300

    Added balance and fix roulette

--- Program.cs
+++ Program.cs
@@ -4,24 +4,17 @@ namespace CasinoWoGUI
   internal class Program
   {
     static Random rnd = new Random();
-    public static int balance;
+    public static double balance;
     public static void game(int games_list)
     {
       if (games_list == 1)
       {
         int bet;
-        Console.WriteLine("\t  1. Ставка на число");
-        Console.WriteLine("\t  2. Ставка на Красное/Черное");
-        Console.Write("Выберите необходимый пункт: ");
-        int roulette_menu = int.Parse(Console.ReadLine());
-        while (roulette_menu < 0 || roulette_menu > 2)
+        string accept = "Stay";
+        while (accept == "Stay")
         {
-          Console.WriteLine("Введенное число должно лежать в промежутке от 1 до 2");
-          Console.Write("Введите число заново: ");
-          roulette_menu = int.Parse(Console.ReadLine());
-        }
-        if (roulette_menu == 1)
-        {
+          Console.Clear();
+          Console.WriteLine("\t  Рулетка");
           Console.Write("Ставка: ");
           bet = int.Parse(Console.ReadLine());
           while (bet > balance)
@@ -30,25 +23,27 @@ namespace CasinoWoGUI
             Console.Write("Введите число заново: ");
             bet = int.Parse(Console.ReadLine());
           }
-          if (balance <= 0d)
+          balance -= bet;
+          Console.Write("Введите число 0-36: ");
+          int n = int.Parse(Console.ReadLine());
+          if (Win(n))
           {
-            Console.WriteLine("Вы все проиграли");
+            balance += bet * 1.95;
+            Console.WriteLine($"Ваш выигрыш составил: {bet * 2}");
           }
           else
           {
-            Console.WriteLine("Введите число 0-36");
-            int n = int.Parse(Console.ReadLine());
-            if (Win(n))
+            balance -= bet;
+            Console.WriteLine($"Ваш проигрыш составил: {bet}");
+            if (balance == 0)
             {
-              balance += bet * 4;
-              Console.WriteLine($"Ваш выигрыш составил: {bet * 4}");
-            }
-            else
-            {
-              balance -= bet;
-              Console.WriteLine($"Ваш проигрыш составил: {bet}");
+              Console.WriteLine("Вы все проиграли!");
             }
           }
+          
+          Console.WriteLine("\nЕсли хотите выйти, введите: Exit");
+          Console.WriteLine("Если хотите остаться, введите: Stay");
+          accept = Console.ReadLine();
         }
       }
     }
@@ -86,36 +81,46 @@ namespace CasinoWoGUI
       Console.Write($"Здравствуйте, {name}, введите Ваш баланс: ");
       balance = int.Parse(Console.ReadLine());
       Console.WriteLine($"\nИгрок: {name}\nБаланс: {balance}");
-      Console.WriteLine("\tМеню: ");
-      Console.WriteLine("\t  1. Список игр");
-      Console.WriteLine("\t  2. Процент выигрышей");
-      Console.WriteLine("\t  3. Время в игре");
-      Console.WriteLine("\t  4. Баланс");
-      Console.Write("Выберите необходимый пункт: ");
-      int menu = int.Parse(Console.ReadLine());
-      while (menu < 0 || menu > 4)
+      while (true)
       {
-        Console.WriteLine("Введенное число должно лежать в промежутке от 1 до 4");
-        Console.Write("Введите число заново: ");
-        menu = int.Parse(Console.ReadLine());
-      }
-      if (menu == 1)
-      {
-        Console.WriteLine("\t  1. Рулетка");
-        Console.WriteLine("\t  2. Больше Меньше");
-        Console.WriteLine("\t  3. Угадай число");
+        
+        Console.WriteLine("\tМеню: ");
+        Console.WriteLine("\t  1. Список игр");
+        Console.WriteLine("\t  2. Процент выигрышей");
+        Console.WriteLine("\t  3. Время в игре");
+        Console.WriteLine("\t  4. Баланс");
         Console.Write("Выберите необходимый пункт: ");
-        int games_list = int.Parse(Console.ReadLine());
-        if (games_list < 0 || games_list > 3)
+        int menu = int.Parse(Console.ReadLine());
+        while (menu < 0 || menu > 4)
         {
-          do
-          {
-            Console.WriteLine("Введенное число должно лежать в промежутке от 1 до 3");
-            Console.Write("Введите число заново: ");
-            games_list = int.Parse(Console.ReadLine());
-          } while (games_list < 0 || games_list > 3);
+          Console.WriteLine("Введенное число должно лежать в промежутке от 1 до 4");
+          Console.Write("Введите число заново: ");
+          menu = int.Parse(Console.ReadLine());
         }
-        game(games_list);
+        if (menu == 1)
+        {
+          Console.WriteLine("\t  1. Рулетка");
+          Console.WriteLine("\t  2. Больше Меньше");
+          Console.WriteLine("\t  3. Угадай число");
+          Console.Write("Выберите необходимый пункт: ");
+          int games_list = int.Parse(Console.ReadLine());
+          if (games_list < 0 || games_list > 3)
+          {
+            do
+            {
+              Console.WriteLine("Введенное число должно лежать в промежутке от 1 до 3");
+              Console.Write("Введите число заново: ");
+              games_list = int.Parse(Console.ReadLine());
+            } while (games_list < 0 || games_list > 3);
+          }
+          game(games_list);
+          Console.Clear();
+        }
+
+        if (menu == 4)
+        {
+          Console.WriteLine($"Ваш баланс: {balance}");
+        }
       }
     }
   } 
